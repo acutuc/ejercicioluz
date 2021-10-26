4697ef6169ffcb215b9d07cc8c2ebf01e96f6fd8
acutuc <acutuc94@gmail.com>
2021-10-26 15:31:31 +0200

    Se sube a github

---
+++ src/main/java/ejercicio/parteA.java
@@ -0,0 +1,12 @@
+
+package ejercicio;
+
+
+public class parteA {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/main/java/ejercicio/parteB.java
@@ -0,0 +1,12 @@
+
+package ejercicio;
+
+
+public class parteB {
+
+   
+    public static void main(String[] args) {
+        
+    }
+    
+}
